806df24ab984184411d5020a7082d2bbc34fabc1
Christopher Stryker <christopher.stryker@emerson.com>
2025-01-27 13:12:58 -0600

    make type checking happy

--- tests/integration/core/test_base_client.py
+++ tests/integration/core/test_base_client.py
@@ -16,6 +16,7 @@ class TestBaseClient:
     def test__get_single_workspace__succeeds(self, client: BaseClient):
         """Test the case of getting a single (Default) workspace."""
         response = client.get_workspaces(["Default"])
+        assert response is not None
         assert response["Default"] is not None
 
     def test__get_all_workspaces__succeeds(self, client: BaseClient):

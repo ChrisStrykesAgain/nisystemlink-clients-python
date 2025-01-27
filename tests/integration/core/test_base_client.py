a437d3d930218216d87a466b1d42821fff0cd4c8
Christopher Stryker <christopher.stryker@emerson.com>
2025-01-27 13:04:19 -0600

    Pass correct type and fix formatting

--- tests/integration/core/test_base_client.py
+++ tests/integration/core/test_base_client.py
@@ -15,7 +15,7 @@ def client(enterprise_config) -> BaseClient:
 class TestBaseClient:
     def test__get_single_workspace__succeeds(self, client: BaseClient):
         """Test the case of getting a single (Default) workspace."""
-        response = client.get_workspaces("Default")
+        response = client.get_workspaces(["Default"])
         assert response["Default"] is not None
 
     def test__get_all_workspaces__succeeds(self, client: BaseClient):
